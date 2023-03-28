4a5bfcb1724d33f47030fd2bcf537e17e507dd17
Maegan Jong <maegan.jong@gmail.com>
2023-03-27 22:59:28 -0400

    starter pt 2

---
+++ commands.py
@@ -0,0 +1,41 @@
+# Connection Data
+HEADER = 64
+PORT = 8500
+FORMAT = 'utf-8'
+## Edit Server below to the hostname of the machine running the server
+SERVER = "dhcp-10-250-18-31.harvard.edu" 
+ADDR = (SERVER, PORT)
+
+# Data Types
+PURPOSE = "!PURPOSE:"
+RECIPIENT = "!RECIPIENT:"
+SENDER = "!SENDER:"
+LENGTH = "!LENGTH:"
+BODY = "!BODY:"
+
+# General
+SEPARATOR = "/"
+MAX_BANDWIDTH = 2048 
+
+# Client Purposes
+CHECK_USER_EXISTS = "!CHECKUSEREXISTS"
+DELETE_ACCOUNT = "!DELETEACCOUNT"
+SHOW_ACCOUNTS = "!SHOWACCOUNTS"
+LOGIN = "!LOGIN"
+REGISTER = "!REGISTER"
+PULL_MESSAGE = "!PULL"
+SEND_MESSAGE = "!SEND"
+
+# Server Purposes
+NO_MORE_DATA = "!NOMOREDATA"
+NOTIFY = "!NOTIFY"
+
+# Printable messages from NOTIFY
+LOGIN_SUCCESSFUL = "Login successful!"
+USER_DOES_NOT_EXIST = "User does not exist."
+DELETION_SUCCESSFUL = "Account deleted."
+DELETION_UNSUCCESSFUL = "Account cannot be deleted."
+LOGOUT_SUCCESSFUL = "Logout successful."
+
+# IDK WHERE THIS ONE IS YET
+DISCONNECT_MESSAGE = "!DISCONNECT"
---
+++ run_chat_server.py
@@ -0,0 +1,5 @@
+from server import *
+
+chat_server = ServerRunner()
+print("[STARTING] server is starting...")
+chat_server.start()
